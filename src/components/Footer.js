c8673f99e60b835ebca10672c28107a3ea789986
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-11-29 17:42:20 -0300

    Requisito 18

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 
@@ -7,13 +7,12 @@ class Footer extends Component {
   render() {
     return (
       <div data-testid="footer" className="footer">
-        {/* <Link to={}> */}
-        {/* <drinkIcon /> */}
-        <img src={ drinkIcon } alt="drink-icon" data-testid="drinks-bottom-btn" />
-        {/* </Link>
-        <Link to={}> */}
-        <img src={ mealIcon } alt="meal-icon" data-testid="meals-bottom-btn" />
-        {/* </Link> */}
+        <Link to="/drinks">
+          <img src={ drinkIcon } alt="drink-icon" data-testid="drinks-bottom-btn" />
+        </Link>
+        <Link to="/meals">
+          <img src={ mealIcon } alt="meal-icon" data-testid="meals-bottom-btn" />
+        </Link>
       </div>
     );
   }
