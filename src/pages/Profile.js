2a28cb93357e2e353f4098a23d2b8e3ac08d6a0a
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-11-29 18:21:14 -0300

    Retornando o estado do release

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,16 +1,6 @@
 function Profile() {
-  const getEmail = JSON.parse(localStorage.getItem('user')).email;
-
-  console.log(getEmail);
   return (
-    <>
-      <h1>Profile</h1>
-      <span>E-mail: </span>
-      <p data-testid="profile-email">{getEmail}</p>
-      <button type="button" data-testid="profile-done-btn">Done Recipes</button>
-      <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
-      <button type="button" data-testid="profile-logout-btn">Logout</button>
-    </>
+    <h1>Profile</h1>
   );
 }
 
