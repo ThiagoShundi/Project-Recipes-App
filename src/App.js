4293e06f8bd77fd888f9f26a6782f0683197e972
ThiagoShundi <shundi_hayashi@hotmail.com>
2022-11-29 15:52:15 -0300

    Ajustando as pastas

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
-  return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
-  );
+return (
+  <Switch>
+    <Route exact path="/" component={ Login } />
+  </Switch>
+);
 }
 
-export default App;
+export default App; 
