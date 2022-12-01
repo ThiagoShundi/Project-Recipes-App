37765dcc60b9ed5fa2ee3d19690fbec99069ef73
LeandroDoudement <leandrodoudement@gmail.com>
2022-12-01 19:03:30 -0300

    Inicio da tela de detalhes

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+// import React, { useEffect, useState } from 'react';
+// import { useParams } from 'react-router-dom';
 
 export default function RecipeDetails() {
-  const { id } = useParams();
-  const [drinkIdState, setDrinkIdState] = useState([]);
-  const [mealsIdState, setMealsIdState] = useState([]);
+  // const { id } = useParams();
+  // const [drinkIdState, setDrinkIdState] = useState([]);
+  // const [mealsIdState, setMealsIdState] = useState([]);
 
-  useEffect();
+  // useEffect();
   return (
     <div>
       <h3 data-testid="recipe-title">Título da receita</h3>
-      <img data-testid="recipe-photo" />
+      {/* <img data-testid="recipe-photo" /> */}
       <h2 data-testid="recipe-category">Categoria da receita</h2>
       <ol>
         <li>Item 1</li>
       </ol>
       <p data-testid="instructions">Instruções</p>
-      <iframe data-testid="video" />
+      {/* <iframe data-testid="video" /> */}
     </div>
   );
 }
