df3c19912491d94b649b1a5749a1ff6b6343e205
Nathalia Jara Cantarelli <108432963+NathaliaJaraCantarelli@users.noreply.github.com>
2022-12-07 19:18:47 -0300

    Ajuste lint

--- src/tests/RecipeDetailsMeals.test.js
+++ src/tests/RecipeDetailsMeals.test.js
@@ -1,7 +1,7 @@
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import RecipeDetailsMeal from '../components/RecipeDetailsMeal';
-// import RecipeDetails from '../pages/RecipeDetails';
+import RecipeDetails from '../pages/RecipeDetails';
 import mockMealDetails from './helpers/mockMealDetails';
 import { renderWithRouter } from './helpers/renderWith';
 
